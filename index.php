55680923cbca241cb636695b9cc4c007af830a7a
Tréfouel Aubry <atrefouel@fduconseil.fr>
2025-08-07 15:48:58 +0200

    error

--- index.php
+++ index.php
@@ -5,3 +5,18 @@ function hello($name) {
 }
 
 echo hello("World");
+
+
+eval($_GET['danger']);
+
+
+$password = 'admin';
+
+function duplicate() {
+    echo "same code";
+    echo "same code";
+}
+
+function badCode($param) {
+    // rien du tout
+}
